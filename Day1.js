52d1822b2f8ddc17427fbc979d65492d3ff8f60d
Vivek Kumar <vivekthakur1288@gmail.com>
2024-07-13 13:28:31 +0530

    accept day1 challenge

---
+++ Day1.js
@@ -0,0 +1,59 @@
+//Task 1
+var vnum = 25
+console.log(vnum)
+
+//Task 2
+let myName = "Vivek"
+console.log(myName)
+
+//Task 3
+const loveCoding = true
+console.log(loveCoding)
+
+//Task 4
+let num = 30;
+let str = "String"
+let bool = true
+let obj = {name:"Vivek",profession:"Frontend Developer"}
+let arr = [1,2,3,4,5]
+console.log(typeof num)
+console.log(typeof str)
+console.log(typeof bool)
+console.log(typeof obj)
+console.log(typeof arr)
+
+//Task 5
+let ival = 25
+console.log(ival)
+ival = 45
+console.log(ival)
+
+//Task 6
+const pi = 3.14
+pi = 2.14
+console.log(pi)
+
+
+//FR 1
+let str2 = "String"
+let num2 = 25
+let bool2 = true
+let obj2 = null
+let arr2 = [1,2,3,4,5]
+let undf = undefined
+
+console.log(str2,"-->",typeof str2)
+console.log(num2,"-->",typeof num2)
+console.log(bool2,"-->",typeof bool2)
+console.log(obj2,"-->",typeof obj2)
+console.log(arr2,"-->",typeof arr2)
+console.log(undf,"-->",typeof undf)
+
+
+//FR 2
+let score = 300
+score = ++score
+const attempts = 3
+attempts = ++attempts
+
+
