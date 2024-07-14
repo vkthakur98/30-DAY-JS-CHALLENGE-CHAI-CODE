bfe17675046c18054fb74b8c7be4524652eb62f7
Vivek Kumar <vivekthakur1288@gmail.com>
2024-07-14 15:44:23 +0530

    accept day2 challenge

---
+++ Day2.js
@@ -0,0 +1,62 @@
+//Activity 1
+//Task 1
+let num1 = 1;
+let num2 = 2;
+let sum = num1 + num2;
+console.log(sum)
+
+//Task 2
+let sub = num1-num2;
+console.log(sub)
+
+//Task 3
+let mul = num1*num2;
+console.log(mul)
+
+//Task 4
+let div = num1/num2;
+console.log(div)
+
+//Task 5
+let rem = num1%num2;
+console.log(rem)
+
+
+// Activity 2
+// Task 6
+let n_num = 0;
+n_num += num1
+console.log(n_num)
+
+// // Task 7
+n_num -= num1
+console.log(n_num)
+
+// Activity 3
+//Task 8
+let cnum1 = 25
+let cnum2 = 45
+let cnum3 = "45"
+    
+//Task 9
+console.log(cnum1>=cnum2)
+console.log(cnum1<=cnum2)
+
+//Task 10
+console.log(cnum2==cnum3)
+console.log(cnum2===cnum3)
+
+// Activity 4
+// Task 11
+let userid = true;
+let pwd = false;
+console.log(userid && pwd)
+
+// Task 12
+console.log(userid || pwd)
+
+// Taks 13
+console.log(!pwd)
+
+// // Task 1
+40>20?console.log("True"):console.log("False");
