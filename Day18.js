bbb60b0f7bf1dd0c207305624c4e40720b5e1821
Vivek Kumar <vivekthakur1288@gmail.com>
2024-08-01 06:44:39 +0530

    accept day18 challenge

--- Day18.js
+++ Day18.js
@@ -3,31 +3,31 @@
 
 let arr = [ 64, 34, 25, 12, 22, 11, 90 ];
 let n = arr.length;
-// function bubbleSort()
-// {
-//     let i, j, temp;
-//     let changed;
-//     for (i = 0; i < n - 1; i++) 
-//     {
-//         changed = false;
-//         for (j = 0; j < n - 1; j++) 
-//         {
-//             if (arr[j] > arr[j + 1]) 
-//             {
-//                 temp = arr[j];
-//                 arr[j] = arr[j + 1];
-//                 arr[j + 1] = temp;
-//                 changed = true;
-//             }
-//         }
-//         if (changed == false)
-//         break;
-//     }
-//     for(let i = 0; i<n; i++){
-//         console.log(arr[i])
-//     }   
-// }
-// bubbleSort();
+function bubbleSort()
+{
+    let i, j, temp;
+    let changed;
+    for (i = 0; i < n - 1; i++) 
+    {
+        changed = false;
+        for (j = 0; j < n - 1; j++) 
+        {
+            if (arr[j] > arr[j + 1]) 
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                changed = true;
+            }
+        }
+        if (changed == false)
+        break;
+    }
+    for(let i = 0; i<n; i++){
+        console.log(arr[i])
+    }   
+}
+bubbleSort();
 
 //Task 2
 function selectionSort()
