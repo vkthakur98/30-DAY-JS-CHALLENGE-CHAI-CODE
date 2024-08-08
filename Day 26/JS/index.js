662594ffac459cadb9a7941e963da04306b7ffc4
Vivek Kumar <vivekthakur1288@gmail.com>
2024-08-08 07:10:21 +0530

    accept day26 challenge

---
+++ Day 26/JS/index.js
@@ -0,0 +1,25 @@
+import { createServer } from 'http';
+import staticHandler from 'serve-handler';
+import ws, { WebSocketServer } from 'ws';
+//serve static folder
+const server = createServer((req, res) => {   // (1)
+    return staticHandler(req, res, { public: 'public' })
+});
+const wss = new WebSocketServer({ server }) // (2)
+wss.on('connection', (client) => {
+    console.log('Client connected !')
+    client.on('message', (msg) => {    // (3)
+        console.log(`Message:${msg}`);
+        broadcast(msg)
+    })
+})
+function broadcast(msg) {       // (4)
+    for (const client of wss.clients) {
+        if (client.readyState === ws.OPEN) {
+            client.send(msg)
+        }
+    }
+}
+server.listen(process.argv[2] || 8080, () => {
+    console.log(`server listening...`);
+})
